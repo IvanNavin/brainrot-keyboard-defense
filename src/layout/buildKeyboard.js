d9d2267a4a555c4979e5d1fb6cadddb10358f63a
Ivan Holovko <e0637242529@gmail.com>
2026-05-02 19:38:03 +0300

    Fix Ukrainian finger guide mapping

--- src/layout/buildKeyboard.js
+++ src/layout/buildKeyboard.js
@@ -24,7 +24,7 @@ export function buildKeyboard(width, height, state) {
       y,
       width: keyWidth,
       height: keyHeight,
-      guide: getFingerGuide(index),
+      guide: getFingerGuide(index, id, state.language),
     }));
   });
 }
--- src/layout/getFingerGuide.js
+++ src/layout/getFingerGuide.js
@@ -1,6 +1,55 @@
 import { COLORS } from "../config.js";
 
-export function getFingerGuide(index) {
+const FINGER_COLORS = {
+  LP: COLORS.fingers.leftPinky,
+  LR: COLORS.fingers.leftRing,
+  LM: COLORS.fingers.leftMiddle,
+  LI: COLORS.fingers.leftIndex,
+  RI: COLORS.fingers.rightIndex,
+  RM: COLORS.fingers.rightMiddle,
+  RR: COLORS.fingers.rightRing,
+  RP: COLORS.fingers.rightPinky,
+};
+
+const UK_FINGER_BY_KEY = {
+  й: "LP",
+  ф: "LP",
+  я: "LP",
+  ц: "LR",
+  і: "LR",
+  ч: "LR",
+  у: "LM",
+  в: "LM",
+  с: "LM",
+  к: "LI",
+  е: "LI",
+  а: "LI",
+  п: "LI",
+  м: "LI",
+  и: "LI",
+  н: "RI",
+  р: "RI",
+  т: "RI",
+  г: "RI",
+  о: "RI",
+  ь: "RI",
+  ш: "RM",
+  л: "RM",
+  б: "RM",
+  щ: "RR",
+  д: "RR",
+  ю: "RR",
+  з: "RP",
+  ж: "RP",
+  х: "RP",
+  є: "RP",
+  ї: "RP",
+};
+
+export function getFingerGuide(index, keyId, language) {
+  const ukFinger = language === "uk" ? UK_FINGER_BY_KEY[keyId] : null;
+  if (ukFinger) return { finger: ukFinger, color: FINGER_COLORS[ukFinger] };
+
   if (index === 0) return { finger: "LP", color: COLORS.fingers.leftPinky };
   if (index === 1) return { finger: "LR", color: COLORS.fingers.leftRing };
   if (index === 2) return { finger: "LM", color: COLORS.fingers.leftMiddle };
